1ac3522b435a5d2c81aabbaf0dc04e2788d0b949
zhengquantao <zhegnquantao80@gmail.com>
2024-05-25 13:07:48 +0800

    update loading ...

--- gui/gauge_frame.py
+++ gui/gauge_frame.py
@@ -2,12 +2,20 @@ import wx
 
 
 class GaugePanel(wx.GenericProgressDialog):
-    def __init__(self, parent, title):
+    def __init__(self, parent, title, callable_func: callable = None, args=None):
         wx.GenericProgressDialog.__init__(self, title, "准备中", maximum=100, parent=parent,
-                                          style=wx.PD_APP_MODAL | wx.PD_ELAPSED_TIME)
+                                          style=wx.PD_APP_MODAL | wx.PD_ELAPSED_TIME | wx.PD_CAN_ABORT)
         self.timer = wx.Timer(self)
         self.Bind(wx.EVT_TIMER, self.update_progress)
         self.timer.Start(600)
+        self.finally_progress(callable_func, args)
+
+    def finally_progress(self, func: callable, args: tuple):
+        try:
+            func(*args)
+            self.Update(100, "加载完成")
+        finally:
+            wx.CallAfter(self.close)
 
     def update_progress(self, event):
         try:
@@ -16,24 +24,15 @@ class GaugePanel(wx.GenericProgressDialog):
                 value += 1
                 self.Update(value, "加载中")
 
-            elif value >= 50 and value < 100:
+            elif value >= 50 and value < 99.7:
                 value += 0.02
                 self.Update(value, "加载中")
 
             else:
-                wx.CallAfter(self.close)
-
-        except:
+                self.Update(value, "加载中")
+        finally:
             pass
-
-    def destroy(self):
-        try:
-            self.Update(100, "加载完成")
-            wx.CallAfter(self.close)
-
-        except Exception as e:
-            print(e)
 
     def close(self):
         self.timer.Stop()
-        wx.CallAfter(self.Destroy)
+        self.Destroy()
